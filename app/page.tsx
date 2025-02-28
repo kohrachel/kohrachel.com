5556f9198258d406587e893a2b2761629458cd99
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-02-27 18:54:06 -0600

    added images

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
 "use client";
 import { Mona_Sans } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
 const panels = [
   {
     name: "agent",
     href: "/agent",
+    imgSrc: "/assets/agent.png",
   },
   {
     name: "world",
-    href: "/",
+    href: "/world",
+    imgSrc: "/assets/world.png",
   },
   {
     name: "traveller",
-    href: "/",
+    href: "/traveller",
+    imgSrc: "/assets/traveller.png",
   },
 ];
 
@@ -24,26 +28,32 @@ export default function Home() {
       <div className="text-center text-8xl px-3 py-10">hello, _____</div>
 
       <div className="p-5 gap-7 h-full flex items-start hover:!opacity-100">
-        {panels.map((panel) => (
-          <Panel name={panel.name} />
+        {panels.map(({ name, href, imgSrc }) => (
+          <Panel name={name} href={href} imgSrc={imgSrc} />
         ))}
       </div>
     </div>
   );
 }
 
-const Panel = ({ name }: { name: string }) => {
+interface PanelProps {
+  name: string;
+  href: string;
+  imgSrc: string;
+}
+
+const Panel = ({ name, href, imgSrc }: PanelProps) => {
   return (
-    <div
+    <Link
+      key={name}
+      href={href}
       className="group grid items-center w-1/3 h-full hover:bg-stone-800 hover:text-4xl duration-700 border border-1 border-zinc-700 outline outline-1 outline-offset-[6px] outline-zinc-800 text-white text-3xl rounded-3xl p-4 text-center align-middle cursor-pointer"
-      onClick={() => {
-        console.log("I was clicked");
-      }}
     >
       {name}
+      <img src={imgSrc} alt={name}></img>
       <div className="text-xl opacity-0 justify-center items-center text-white group-hover:!opacity-100 duration-700">
         This is some text you only see if you hover
       </div>
-    </div>
+    </Link>
   );
 };
