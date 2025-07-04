8f8d00b1fdb2aa7f47edbf063a21efa521941bfe
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-07-04 17:05:12 -0400

    Remove Link (from Footer) for compilation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Montserrat, EB_Garamond } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
