2cc7941f3da2de9def4cdffbb19ebf60cca31056
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-07-05 20:27:54 -0400

    Scaffolding blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function Blog() {
+  return (
+    <div className="w-screen h-screen bg-yellow-300">
+      hello! so excited that you're here
+      <div className=""></div>
+    </div>
+  );
+}
