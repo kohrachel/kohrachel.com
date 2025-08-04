552e888a79626489970a022e86aa8420be81bdef
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-03 21:30:22 -0400

    Don't remember what I was doing here but oh well

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -49,20 +49,20 @@ export default function Blog() {
           <div className="flex flex-col justify-between w-stretch h-full px-24 py-16 rounded-lg outline outline-purple-200 bg-white overflow-y-scroll">
             <div className="grid grid-cols-2 gap-x-12 gap-y-9">
               {posts.map((post) => (
-                <Link href="" key={post.title} className="rounded-2xl">
-                  <div className="flex flex-col relative justify-between group rounded-2xl h-max m-1 outline outline-purple-200 outline-offset-4 hover:outline-purple-400 transition-all duration-500">
-                    <div className="absolute text-xs uppercase top-2 left-2 text-stone-500">
-                      {post.date}
-                    </div>
-                    <div className="absolute bottom-0 left-0 right-0 flex flex-col gap-1 backdrop-blur-md pl-3 py-2 pr-11 rounded-b-2xl border-solid border-purple-50 border-t-[1px] border-opacity-20">
-                      <p className="text-3xl group-hover:text-purple-800 font-serif font-bold italic transition-all duration-300">
+                <Link href="" key={post.title} className="rounded-lg">
+                  <div className="flex flex-col gap-1 relative justify-between group rounded-lg h-max m-1 outline outline-purple-200 outline-offset-4 hover:outline-purple-400 transition-all duration-500">
+                    <img src={post.imgPath} className="rounded-lg" />
+                    <div className="p-2">
+                      <p className="text-xs uppercase text-stone-500 group-hover:text-purple-600">
+                        {post.date}
+                      </p>
+                      <p className="text-3xl group-hover:text-purple-800 py-1 font-serif font-bold italic transition-all duration-300">
                         {post.title}
                       </p>
-                      <p className="text-stone-900 text-sm group-hover:text-md transition-all duration-300">
+                      <p className="text-stone-900 group-hover:text-purple-600 text-sm group-hover:text-md transition-all duration-300">
                         {post.description}
                       </p>
                     </div>
-                    <img src={post.imgPath} className="rounded-2xl" />
                   </div>
                 </Link>
               ))}
@@ -86,41 +86,41 @@ const posts: {
     href: "/you-dont-exist",
     description: "the book that just saved me that you haven't heard of",
     date: "2024-01-01",
-    imgPath: "/assets/you-dont-exist.png",
+    imgPath: "/assets/blog/example43.jpg",
   },
   {
     title: "blog about a blog",
     href: "/blog-about-a-blog",
     description: "i sense a glitch in the matrix",
     date: "2025-02-18",
-    imgPath: "/assets/you-dont-exist.png",
+    imgPath: "/assets/blog/example43.jpg",
   },
   {
     title: "5 scrapped NYT bestsellers",
     href: "/5-scrapped-nyt-bestsellers",
     description: "sometimes you should get an editor",
     date: "2025-02-18",
-    imgPath: "/assets/example43.jpg",
+    imgPath: "/assets/blog/example43.jpg",
   },
   {
     title: "ete",
     href: "/ete",
     description: "written purely out of self-indulgence",
     date: "2024-01-04",
-    imgPath: "/assets/example43.jpg",
+    imgPath: "/assets/blog/example43.jpg",
   },
   {
     title: "a series of goodbyes",
     href: "/",
     description: "this is not sappy at all",
     date: "2024-01-04",
-    imgPath: "/assets/example43.jpg",
+    imgPath: "/assets/blog/example43.jpg",
   },
   {
     title: "life (is a stained glass window)",
     href: "/",
     description: "and hopefully not in the wcs way",
     date: "2024-01-04",
-    imgPath: "/assets/example43.jpg",
+    imgPath: "/assets/blog/example43.jpg",
   },
 ];
