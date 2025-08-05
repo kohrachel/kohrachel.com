82b651945bb04213b3d9c3c0737d6780b02ae66c
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-05 16:08:40 -0400

    Good things take time

--- app/second-chances/page.tsx
+++ app/second-chances/page.tsx
@@ -1,7 +1,10 @@
 import Terminal from "@/components/Terminal";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 export default function SecondChances() {
+  redirect("/");
+
   return (
     <Suspense fallback={<div>Loading terminal...</div>}>
       <Terminal />
