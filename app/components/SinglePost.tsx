ba0e6b6e8b13f6e8bb013f749713d027e492d2c3
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-04 23:18:32 -0400

    Use <Title />, <Heading />, and <Paragraph />

--- app/components/SinglePost.tsx
+++ app/components/SinglePost.tsx
@@ -1,22 +1,32 @@
 "use client";
 import { postType } from "../blog/data";
 import React from "react";
+import { Title, Heading, Paragraph } from "./blog-components";
 
 export default function SinglePost({ post }: { post: postType }) {
+  const contentArray = post.content.split("\n");
+
   return (
     <div className="w-full h-full p-8 overflow-auto">
       <div className="flex flex-col items-center gap-1 pb-9">
-        <p className="text-stone-900 text-6xl font-serif font-bold italic pb-3">
-          {post.title}
-        </p>
+        <Title>{post.title}</Title>
         <p className="text-xl">{post.description}</p>
         <p className="text-sm text-stone-500">{post.date}</p>
       </div>
-      <div className="flex flex-col gap-2">
-        {post.content.split("\n").map((chunk, index) => (
-          <p key={index}>{chunk}</p>
+      <div className="flex flex-col gap-3">
+        {contentArray.map((chunk, index) => (
+          <RenderedChunk key={index} chunk={chunk} />
         ))}
       </div>
     </div>
   );
+}
+
+function RenderedChunk({ chunk }: { chunk: string }) {
+  const trimmedChunk = chunk.trim();
+  if (trimmedChunk.startsWith("#")) {
+    return <Heading>{trimmedChunk.substring(1).trim()}</Heading>;
+  } else {
+    return <Paragraph>{chunk}</Paragraph>;
+  }
 }
