eff45aaefab23d48aeed6c0d6e5b3fb9871368de
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-05 15:19:03 -0400

    (suspenseful music plays) Let's actually not nuke the home page and put this in its own page instead (suspenseful music fades to calming tune)

---
+++ app/second-chances/page.tsx
@@ -0,0 +1,10 @@
+import Terminal from "@/components/Terminal";
+import { Suspense } from "react";
+
+export default function SecondChances() {
+  return (
+    <Suspense>
+      <Terminal />;
+    </Suspense>
+  );
+}
