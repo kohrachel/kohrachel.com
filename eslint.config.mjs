175397d0e8851315aeffc464d3ac189cc5b0a184
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-05 17:57:59 -0400

    I think the extends need the specific next config names

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,17 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next", "prettier"],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
   }),
   {
     rules: {
