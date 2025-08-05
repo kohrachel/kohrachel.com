dc0e58509a267bd782fc8c69c9bad3fd4ce541b2
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-05 16:57:00 -0400

    Rename param to text for clarity

--- app/utils.ts
+++ app/utils.ts
@@ -2,7 +2,7 @@ export function first<T>(array: T[], n: number = 1) {
   return array.slice(0, n);
 }
 
-export function getFirstWord(string: string) {
-  if (!string) return "";
-  return string.trim().split(/\s+/)[0] || "";
+export function getFirstWord(text: string) {
+  if (!text) return "";
+  return text.trim().split(/\s+/)[0] || "";
 }
