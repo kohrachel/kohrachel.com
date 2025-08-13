2cb5428999348afe7dea3a77076e226f2145ef80
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-13 13:02:39 -0400

    Apply Dracula theme

---
+++ app/internal/code/page.tsx
@@ -0,0 +1,11 @@
+import { Sandpack } from "@codesandbox/sandpack-react";
+import { Dracula } from "./sandbox-theme";
+export default function App() {
+  return (
+    <div className="p-5">
+      <Sandpack
+        theme={Dracula}
+      />
+    </div>
+  );
+}
