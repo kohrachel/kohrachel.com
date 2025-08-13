02f953bb32ac2e323fc2ec3b3bb2debb5bbe28d1
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-13 13:03:01 -0400

    Use react template

--- app/internal/code/page.tsx
+++ app/internal/code/page.tsx
@@ -1,10 +1,12 @@
 import { Sandpack } from "@codesandbox/sandpack-react";
 import { Dracula } from "./sandbox-theme";
+
 export default function App() {
   return (
     <div className="p-5">
       <Sandpack
         theme={Dracula}
+        template="react"
       />
     </div>
   );
