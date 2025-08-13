beb31b8f2fb78100ff0723937ea28a453d41f96e
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-13 13:04:16 -0400

    Test out multiple tabs display and make editor taller

--- app/internal/code/page.tsx
+++ app/internal/code/page.tsx
@@ -7,6 +7,19 @@ export default function App() {
       <Sandpack
         theme={Dracula}
         template="react"
+        files={{
+          "/App.js": {
+            code: `export default function App() { return <h1>Hello World!</h1>; }`,
+            active: true, // This file will be the active tab
+          },
+          "/index.js": `import App from "./App";`,
+          "/styles.css": `body { font-family: sans-serif; }`,
+        }}
+        options={{
+          showInlineErrors: true, // default - false
+          wrapContent: true, // default - false
+          editorHeight: 700, // default - 300
+        }}
       />
     </div>
   );
