491110a03c966d3a35fb5ec7d53783e80c382867
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-09-06 16:34:09 -0500

    add padding and ensure mobile compatability

--- app/diamond/page.tsx
+++ app/diamond/page.tsx
@@ -1,6 +1,6 @@
 export default function Diamond() {
   return (
-    <div className="bg-black w-full h-screen text-white flex flex-col justify-center text-center items-center">
+    <div className="bg-black w-full h-screen text-white flex flex-col justify-center text-center items-center xl:p-32 p-4">
       <p>hello i'm diamond</p>
       <img
         src="/assets/diamond-stars.png"
