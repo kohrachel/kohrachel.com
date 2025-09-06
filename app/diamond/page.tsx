9f67a1e392a78c2e0140d55555340ac7869cc433
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-09-06 16:29:13 -0500

    scaffold

--- app/diamond/page.tsx
+++ app/diamond/page.tsx
@@ -1,3 +1,13 @@
 export default function Diamond() {
-  return <div></div>;
+  return (
+    <div className="bg-black w-full h-screen text-white flex flex-col justify-center text-center items-center">
+      <p>hello i'm diamond</p>
+      <img
+        src="/assets/diamond-stars.png"
+        alt="Diamond stars"
+        className="h-1/2"
+      />
+      <p>hello i'm diamond</p>
+    </div>
+  );
 }
