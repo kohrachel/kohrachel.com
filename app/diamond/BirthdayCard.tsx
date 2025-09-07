02090952a5dece98316c5b329349d3d5ee8d9b2e
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-09-07 00:56:02 -0500

    diamond and star kinda cute

--- app/diamond/BirthdayCard.tsx
+++ app/diamond/BirthdayCard.tsx
@@ -1,3 +1,110 @@
 export default function BirthdayCard() {
-  return <div>birthday card</div>;
+  return (
+    <div className="w-full h-screen bg-black text-white">
+      <div className="z-10 relative overflow-y-scroll h-full px-80 py-36">
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+        <p>
+          hello i am s ome text can you see me hello i am some text can you see
+          me hello i am some text can you see me hello i am some text can you
+          see me hello i am some text can you see me hello i am some text can
+          you see me{" "}
+        </p>
+      </div>
+      <div
+        className="absolute top-0 left-72 h-36 w-full z-10 backdrop-blur-md bg-gradient-to-t"
+        style={{
+          background:
+            "linear-gradient(rgba(0, 0, 0, 1), rgba(255, 255, 255, 0))",
+        }}
+      ></div>
+      <div
+        className="absolute bottom-0 right-80 h-36 w-full z-10 backdrop-blur-md"
+        style={{
+          background:
+            "linear-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, 1))",
+        }}
+      ></div>
+      <img
+        src="/assets/diamond-star-border-top.png"
+        alt="diamonds, stars, and pearls in a cartoon 3d style"
+        className="absolute top-0 left-0 w-48 lg:w-72 xl:w-96 z-20"
+      />
+      <img
+        src="/assets/diamond-star-border-bottom.png"
+        alt="diamonds, stars, and pearls in a cartoon 3d style"
+        className="absolute bottom-0 right-0 w-48 lg:w-72 xl:w-96 z-20"
+      />
+    </div>
+  );
 }
