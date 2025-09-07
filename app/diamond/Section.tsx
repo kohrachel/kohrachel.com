5bf136af96f72ed08b846375434ca68fbff5ef6c
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-09-06 21:11:54 -0500

    prevent the Section text from overlapping with the nav buttons

--- app/diamond/Section.tsx
+++ app/diamond/Section.tsx
@@ -6,14 +6,9 @@ export type SectionContents = {
   children: string;
 };
 
-// export type SectionProps = SectionContents & {
-//   section: number;
-//   setSection: Dispatch<SetStateAction<number>>;
-// };
-
 export function Section({ title, imgSrc, children }: SectionContents) {
   return (
-    <div className="flex flex-col text-center items-center h-[90%]">
+    <div className="flex flex-col text-center items-center h-[85%]">
       <h1 className="text-3xl font-serif italic font-semibold p-5">{title}</h1>
       <img
         src={imgSrc}
