6ae85159acc5b863df9c90dec7a57d047c4e7c58
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-09-08 14:54:59 -0500

    i can at least pretend that im an engineer

--- app/diamond/BirthdayCard.tsx
+++ app/diamond/BirthdayCard.tsx
@@ -5,36 +5,9 @@ export default function BirthdayCard() {
         <p className="font-serif font-semibold text-3xl italic">
           Happy Birthday Mummy!
         </p>
-        <p>
-          hello i am s ome text can you see me hello i am some text can you see
-          me hello i am some text can you see me hello i am some text can you
-          see me hello i am some text can you see me hello i am some text can
-          you see me{" "}
-        </p>
-        <p>
-          hello i am s ome text can you see me hello i am some text can you see
-          me hello i am some text can you see me hello i am some text can you
-          see me hello i am some text can you see me hello i am some text can
-          you see me{" "}
-        </p>
-        <p>
-          hello i am s ome text can you see me hello i am some text can you see
-          me hello i am some text can you see me hello i am some text can you
-          see me hello i am some text can you see me hello i am some text can
-          you see me{" "}
-        </p>
-        <p>
-          hello i am s ome text can you see me hello i am some text can you see
-          me hello i am some text can you see me hello i am some text can you
-          see me hello i am some text can you see me hello i am some text can
-          you see me{" "}
-        </p>
-        <p>
-          hello i am s ome text can you see me hello i am some text can you see
-          me hello i am some text can you see me hello i am some text can you
-          see me hello i am some text can you see me hello i am some text can
-          you see me{" "}
-        </p>
+        {message.split("\n").map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
         <p className="font-serif font-semibold italic text-xl text-end">
           -- Rachel
         </p>
@@ -66,3 +39,10 @@ export default function BirthdayCard() {
     </div>
   );
 }
+
+const message: string = `Yes, I may have called you a rock. But I called you a special rock okay, plus if you like diamonds so much you can’t possibly hate the thing that the diamonds live in… right? 
+I did a little bit of research into diamonds and realized that wow, it’s (almost) the same thing as graphite. I was very tempted to get you some pencil leads with a note saying sorry for messing up the pressure setting. See I’m so nice I didn’t even do that. (I’ll save that for next year instead).
+As you may be able to tell, I decided to make the theme of this “card” diamonds. I ended up researching how diamonds are created. I knew that diamonds were made under high pressure, but I didn’t know much other than that. 
+Did you know that diamond is actually really abundant in the universe? Maybe we should go support Elon Musk’s obsession with space a little more. 
+In all seriousness, I’ve never met someone as strong and intelligent as you. I’ve never met someone who knows so much about the world, from science to history to psychology. 
+You said that you were never the brightest student among your friends, but you set me up to succeed among mine. You may have forgotten to drill into me how to actually make those friends, but it’s okay, I have Sky. And you love all the stuffed toy animals even if you’ll never admit it. And honestly, I still don’t understand how your friend group stays together after so long. I barely text my friends even with the 5 million different messaging apps I have accounts on. `;
