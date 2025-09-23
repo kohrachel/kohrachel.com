395702a0ef5b475c87b46bb700af30ce4012d726
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-09-22 22:22:18 -0500

    update nextjs config file to configure code-inspector

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { codeInspectorPlugin } from "code-inspector-plugin";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -5,6 +6,15 @@ const nextConfig: NextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  experimental: {
+    turbo: {
+      rules: codeInspectorPlugin({
+        bundler: "turbopack",
+        editor: "cursor",
+        hotKeys: ["altKey"],
+      }),
+    },
   },
 };
 
