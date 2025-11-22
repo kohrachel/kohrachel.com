5269a6e9f6a97a8f84e01cfc3b957681f036cc03
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-11-22 17:15:35 -0500

    tapping a chocolate square unveils either a chest, gems, or a busted X

--- app/chocolate/page.tsx
+++ app/chocolate/page.tsx
@@ -1,8 +1,29 @@
 "use client";
 import { useState } from "react";
+
+type ItemType = "chest" | "gems" | "busted";
+
 export default function Chocolate() {
   const [showSquare, setShowSquare] = useState<boolean[]>(Array(16).fill(true));
   const array = (n: number) => Array.from({ length: n }, (_, index) => index);
+  const ITEMS: ItemType[] = [
+    "chest",
+    "chest",
+    "chest",
+    "chest",
+    "gems",
+    "gems",
+    "gems",
+    "gems",
+    "busted",
+    "busted",
+    "busted",
+    "busted",
+    "chest",
+    "chest",
+    "chest",
+    "chest",
+  ];
 
   const handleClick = (index: number) => {
     setShowSquare((prev) => [
@@ -28,7 +49,18 @@ export default function Chocolate() {
         alt="chocolate"
         className="absolute h-80 object-cover"
       />
-      <div className="absolute h-72 w-72 p-1 rounded-sm object-cover grid grid-rows-4 grid-cols-4 bg-[#4f2c1c]">
+
+      <div className="absolute h-72 w-72 p-1 rounded-sm object-cover grid grid-rows-4 grid-cols-4 items-center justify-items-center bg-[#4f2c1c]">
+        {ITEMS.map((item, index) => (
+          <img
+            key={index}
+            src={`/assets/${item}.png`}
+            alt="chocolate"
+            className={`max-h-12 max-w-12 object-cover`}
+          />
+        ))}
+      </div>
+      <div className="absolute h-72 w-72 p-1 grid grid-rows-4 grid-cols-4">
         {array(16).map((index) => (
           <img
             key={index}
